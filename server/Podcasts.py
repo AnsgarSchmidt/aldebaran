e3222ce9a3ab9b7aad1097f15a9728faf6ad856c
Ansgar Schmidt <ansgar.schmidt@msasafety.com>
2015-04-28 21:00:00 +0200

    Changing data layout for podcasts to support max and part

--- server/Podcasts.py
+++ server/Podcasts.py
@@ -2,62 +2,70 @@ import socket
 import feedparser
 import random
 
-podcastLinks = ['http://www.deutschlandfunk.de/podcast-forschung-aktuell.677.de.podcast.xml',
-                'http://www.deutschlandfunk.de/podcast-buechermarkt.701.de.podcast.xml',
-                'http://www.deutschlandfunk.de/podcast-campus-karriere.681.de.podcast.xml',
-                'http://www.deutschlandfunk.de/podcast-computer-und-kommunikation.685.de.podcast.xml',
-                'http://www.deutschlandfunk.de/podcast-informationen-am-abend.791.de.podcast.xml',
-                'http://www.fritz.de/media/podcasts/serien/fritzfressefreiheit.feed.podcast.xml',
-                'http://www.fritz.de/media/podcasts/serien/neues_vom_kaenguru_podcast.feed.podcast.xml',
-                'http://www.fritz.de/media/podcasts/fritzinfo/multimedia_und_games.feed.podcast.xml',
-                'http://www.fritz.de/media/podcasts/fritzinfo/buecher.feed.podcast.xml',
-                'http://www.fritz.de/media/podcasts/fritzinfo/neues-aus-der-netzwelt.feed.podcast.xml',
-                'http://www.fritz.de/media/podcasts/fritzinfo/film.feed.podcast.xml',
-                'http://www.fritz.de/media/podcasts/fritzinfo/platten.feed.podcast.xml',
-                'http://www1.wdr.de/radio/podcasts/wdr5/satirischewochenrueckblick100.podcast',
-                # TO LONG 'http://www1.wdr.de/radio/podcasts/wdr5/dasphilosophischeradio100.podcast',
-                'http://downloads.bbc.co.uk/podcasts/radio/stargazing/rss.xml',
-                'http://downloads.bbc.co.uk/podcasts/radio4/myshakespeare/rss.xml',
-                'http://downloads.bbc.co.uk/podcasts/worldservice/6min_vocab/rss.xml',
-                'http://downloads.bbc.co.uk/podcasts/worldservice/6min_gram/rss.xml',
-                'http://downloads.bbc.co.uk/podcasts/worldservice/tae/rss.xml',
-                'http://downloads.bbc.co.uk/podcasts/worldservice/how2/rss.xml',
-                'http://downloads.bbc.co.uk/podcasts/worldservice/elt_drama/rss.xml',
-                'http://www.ndr.de/ndr2/programm/podcast2966.xml',
-                'http://www.ndr.de/ndr2/programm/podcast2968.xml',
-                'http://www.ndr.de/ndr2/fruehstueck_bei_stefanie/podcast2956.xml',
-                'http://www.n-joy.de/podcast/podcast4169.xml',
-                'http://www.n-joy.de/podcast/podcast4167.xml',
-                'http://www.ndr.de/info/podcast3002.xml',
-                'http://www.ndr.de/ndrkultur/rezensionen/podcast3028.xml',
-                'http://www.ndr.de/ndrkultur/rezensionen/podcast3036.xml',
-                'http://www.ndr.de/ndrkultur/rezensionen/podcast3024.xml',
-                'http://www.ndr.de/ndrkultur/rezensionen/podcast3022.xml',
-                'http://www.ndr.de/radiomv/podcast3080.xml',
-                'http://www1.swr.de/podcast/xml/swr1/bw/swr1-durchgeschroedert.xml',
-                'http://www1.swr.de/podcast/xml/swr2/99sekunden.xml',
-                'http://www1.swr.de/podcast/xml/swr2/buch-der-woche.xml',
-                'http://www.inforadio.de/podcast/feeds/wissenswerte/wissenswerte.xml/feed=podcast.xml',
-                'http://www1.wdr.de/radio/podcasts/fhe/alltagosman102.podcast',
-                'http://www.ndr.de/podcast/podcast2956.xml',
-                'http://arvid9.hipcast.com/rss/braincast.xml',
-                'http://www.fraunhofer.de/de/rss/podcast.rss',
-                'http://www1.swr.de/podcast/xml/swr1/rp/stenkelfeld.xml',
-                'http://www1.wdr.de/radio/podcasts/wdr2/kino190.podcast',
-                # To long 'http://www.davidbarrkirtley.com/podcast/geeksguideshow.xml',
-                # To long 'http://www.howstuffworks.com/podcasts/stuff-you-should-know.rss',
-                'http://rss.sciam.com/sciam/60secsciencepodcast?format=xml'
-]
+ALL = 0
+TOP = 1
+END = 2
+
+
+podcastLinks = [
+                [10, TOP, 'http://www.deutschlandfunk.de/podcast-forschung-aktuell.677.de.podcast.xml'],
+                [10, TOP, 'http://www.deutschlandfunk.de/podcast-buechermarkt.701.de.podcast.xml'],
+                [10, TOP, 'http://www.deutschlandfunk.de/podcast-campus-karriere.681.de.podcast.xml'],
+                [10, TOP, 'http://www.deutschlandfunk.de/podcast-computer-und-kommunikation.685.de.podcast.xml'],
+                [10, TOP, 'http://www.deutschlandfunk.de/podcast-informationen-am-abend.791.de.podcast.xml'],
+                [10, TOP, 'http://www.fritz.de/media/podcasts/serien/fritzfressefreiheit.feed.podcast.xml'],
+                [10, TOP, 'http://www.fritz.de/media/podcasts/serien/neues_vom_kaenguru_podcast.feed.podcast.xml'],
+                [10, TOP, 'http://www.fritz.de/media/podcasts/fritzinfo/multimedia_und_games.feed.podcast.xml'],
+                [10, TOP, 'http://www.fritz.de/media/podcasts/fritzinfo/buecher.feed.podcast.xml'],
+                [10, TOP, 'http://www.fritz.de/media/podcasts/fritzinfo/neues-aus-der-netzwelt.feed.podcast.xml'],
+                [10, TOP, 'http://www.fritz.de/media/podcasts/fritzinfo/film.feed.podcast.xml'],
+                [10, TOP, 'http://www.fritz.de/media/podcasts/fritzinfo/platten.feed.podcast.xml'],
+                [10, TOP, 'http://www1.wdr.de/radio/podcasts/wdr5/satirischewochenrueckblick100.podcast'],
+                [ 0, TOP, 'http://www1.wdr.de/radio/podcasts/wdr5/dasphilosophischeradio100.podcast'],
+                [10, TOP, 'http://downloads.bbc.co.uk/podcasts/radio/stargazing/rss.xml'],
+                [10, TOP, 'http://downloads.bbc.co.uk/podcasts/radio4/myshakespeare/rss.xml'],
+                [10, TOP, 'http://downloads.bbc.co.uk/podcasts/worldservice/6min_vocab/rss.xml'],
+                [10, TOP, 'http://downloads.bbc.co.uk/podcasts/worldservice/6min_gram/rss.xml'],
+                [10, TOP, 'http://downloads.bbc.co.uk/podcasts/worldservice/tae/rss.xml'],
+                [10, TOP, 'http://downloads.bbc.co.uk/podcasts/worldservice/how2/rss.xml'],
+                [10, TOP, 'http://downloads.bbc.co.uk/podcasts/worldservice/elt_drama/rss.xml'],
+                [10, TOP, 'http://www.ndr.de/ndr2/programm/podcast2966.xml'],
+                [10, TOP, 'http://www.ndr.de/ndr2/programm/podcast2968.xml'],
+                [10, TOP, 'http://www.ndr.de/ndr2/fruehstueck_bei_stefanie/podcast2956.xml'],
+                [10, TOP, 'http://www.n-joy.de/podcast/podcast4169.xml'],
+                [10, TOP, 'http://www.n-joy.de/podcast/podcast4167.xml'],
+                [10, TOP, 'http://www.ndr.de/info/podcast3002.xml'],
+                [10, TOP, 'http://www.ndr.de/ndrkultur/rezensionen/podcast3028.xml'],
+                [10, TOP, 'http://www.ndr.de/ndrkultur/rezensionen/podcast3036.xml'],
+                [10, TOP, 'http://www.ndr.de/ndrkultur/rezensionen/podcast3024.xml'],
+                [10, TOP, 'http://www.ndr.de/ndrkultur/rezensionen/podcast3022.xml'],
+                [10, TOP, 'http://www.ndr.de/radiomv/podcast3080.xml'],
+                [10, TOP, 'http://www1.swr.de/podcast/xml/swr1/bw/swr1-durchgeschroedert.xml'],
+                [10, TOP, 'http://www1.swr.de/podcast/xml/swr2/99sekunden.xml'],
+                [10, TOP, 'http://www1.swr.de/podcast/xml/swr2/buch-der-woche.xml'],
+                [10, TOP, 'http://www.inforadio.de/podcast/feeds/wissenswerte/wissenswerte.xml/feed=podcast.xml'],
+                [10, TOP, 'http://www1.wdr.de/radio/podcasts/fhe/alltagosman102.podcast'],
+                [10, TOP, 'http://www.ndr.de/podcast/podcast2956.xml'],
+                [10, TOP, 'http://arvid9.hipcast.com/rss/braincast.xml'],
+                [10, TOP, 'http://www.fraunhofer.de/de/rss/podcast.rss'],
+                [10, TOP, 'http://www1.swr.de/podcast/xml/swr1/rp/stenkelfeld.xml'],
+                [10, TOP, 'http://www1.wdr.de/radio/podcasts/wdr2/kino190.podcast'],
+                [ 0, TOP, 'http://www.davidbarrkirtley.com/podcast/geeksguideshow.xml'],
+                [ 0, TOP, 'http://www.howstuffworks.com/podcasts/stuff-you-should-know.rss'],
+                [10, TOP, 'http://rss.sciam.com/sciam/60secsciencepodcast?format=xml']
+               ]
 
 class Podcast:
 
-    def __init__(self, url):
+    def __init__(self, maxEpisodes, part, url):
         if url == None or len(url) == 0:
             raise Exception('No valid URL')
-        self._url    = url
-        self._name   = 'NOT_SET'
-        self._links  = []
-        self._active = False
+        self._url         = url
+        self._maxEpisodes = maxEpisodes
+        self._part        = part
+        self._name        = 'NOT_SET'
+        self._links       = []
+        self._active      = False
         socket.setdefaulttimeout(2)
         self.refresh()
 
@@ -88,12 +96,14 @@ class Podcast:
                     self._links.append(links['href'])
         print "%d episodes at %s"%(len(self._links), self._name)
 
+
+
 class Podcasts:
 
     def __init__(self):
         self._podcasts = {}
-        for podcastLink in podcastLinks:
-            p = Podcast(podcastLink)
+        for maxEpisodes, part, podcastLink in podcastLinks:
+            p = Podcast(maxEpisodes, part, podcastLink)
             self._podcasts[p.getName()] = p
 
     def setActive(self, key, value):
